7e16744366747f8af40fac764b4a0a681fd49ad9
Saquib <mdsaquib4292@gmail.com>
2026-01-04 16:19:53 +0530

    Update backend production URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://collaborative-task-manager-2.onrender.com/",
 });
 
 api.interceptors.request.use((config) => {
